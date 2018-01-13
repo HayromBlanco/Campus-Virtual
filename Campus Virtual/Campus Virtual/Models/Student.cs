327ebf99abc39299fc7c4631b44cf9f24ee1a88a
NAYTHAS\ExtremeTech <sebasbreness@gmail.com>
2018-01-12 21:55:47 -0600

    Prueba Merge Student

---
+++ Campus Virtual/Campus Virtual/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Campus_Virtual.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+
+
+    }
+}
