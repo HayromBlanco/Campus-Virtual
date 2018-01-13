c3c65ebafa81dd1dec52ff413df427f3059859df
DESKTOP-5S9E316\Personal <juanjo.rs14@hotmail.com>
2018-01-12 21:23:01 -0600

    Se agrego clase Curso

---
+++ Campus Virtual/Campus Virtual/Models/Cursos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Campus_Virtual.Models
+{
+    public class Cursos
+    {
+        private int CurseID { get; set; } 
+
+    }
+}
